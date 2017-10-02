413f45b44d5ab33f9d2aa1d94a36c4d2a7e4a896
Charles Shane <chsh@panduit.com>
2017-10-02 11:21:14 -0500

    whitespace removal for CI testing.

--- src/main/java/com/mulesoft/training/Database.java
+++ src/main/java/com/mulesoft/training/Database.java
@@ -5,7 +5,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
